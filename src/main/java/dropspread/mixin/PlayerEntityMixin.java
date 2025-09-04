c8c76a02af69a44818b0b0db720dc7a911a30e60
T0RNATO <nlittle315@gmail.com>
2025-09-04 17:38:20 +1000

    1.21.6

--- src/main/java/dropspread/mixin/PlayerEntityMixin.java
+++ src/main/java/dropspread/mixin/PlayerEntityMixin.java
@@ -1,25 +1,27 @@
 package dropspread.mixin;
 
 import dropspread.DropSpread;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-@Mixin(Player.class)
+@Mixin(LivingEntity.class)
 public abstract class PlayerEntityMixin extends Entity {
 	public PlayerEntityMixin(EntityType<?> type, Level level) {
 		super(type, level);
 	}
 	@ModifyArgs(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/ItemEntity;setDeltaMovement(DDD)V", ordinal = 0),
-			method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;")
+			method = "createItemStackToDrop")
 	private void init(Args args) {
 		Level level = this.level();
-		double spread = level.getGameRules().getRule(DropSpread.DROP_SPREAD).get();
+		if (level.isClientSide()) return;
+		double spread = ((ServerLevel)level).getGameRules().getRule(DropSpread.DROP_SPREAD).get();
 
 		for (int i = 0; i < args.size(); i++)
 			args.set(i, (double) args.get(i) * spread);
