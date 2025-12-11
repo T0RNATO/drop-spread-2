0dc809c51d43b61facd9e4aac6691ca6cae02a25
T0RNATO <nlittle315@gmail.com>
2025-12-11 17:30:30 +1100

    1.21.11

--- src/main/java/dropspread/DropSpread.java
+++ src/main/java/dropspread/DropSpread.java
@@ -2,17 +2,16 @@ package dropspread;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
-import net.fabricmc.fabric.api.gamerule.v1.rule.DoubleRule;
-import net.minecraft.world.level.GameRules;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleBuilder;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.gamerules.GameRule;
+import net.minecraft.world.level.gamerules.GameRuleCategory;
 
 public class DropSpread implements ModInitializer {
-	public static GameRules.Key<DoubleRule> DROP_SPREAD = GameRuleRegistry.register(
-			"dropSpread",
-			GameRules.Category.DROPS,
-			GameRuleFactory.createDoubleRule(0.1D, 0, 10)
-	);
+	public static GameRule<Double> DROP_SPREAD = GameRuleBuilder.forDouble(0.1)
+			.category(GameRuleCategory.DROPS)
+			.range(0d, 10d)
+			.buildAndRegister(Identifier.fromNamespaceAndPath("drop_spread", "drop_spread"));
 
 	@Override
 	public void onInitialize() {
--- src/main/java/dropspread/mixin/PlayerEntityMixin.java
+++ src/main/java/dropspread/mixin/PlayerEntityMixin.java
@@ -1,8 +1,10 @@
 package dropspread.mixin;
 
 import dropspread.DropSpread;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,16 +12,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-@Mixin(Player.class)
+@Mixin(LivingEntity.class)
 public abstract class PlayerEntityMixin extends Entity {
 	public PlayerEntityMixin(EntityType<?> type, Level level) {
 		super(type, level);
 	}
-	@ModifyArgs(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/ItemEntity;setDeltaMovement(DDD)V", ordinal = 0),
-			method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;")
+
+	@ModifyArgs(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/ItemEntity;setDeltaMovement(DDD)V", ordinal = 0), method = "createItemStackToDrop")
 	private void init(Args args) {
-		Level level = this.level();
-		double spread = level.getGameRules().getRule(DropSpread.DROP_SPREAD).get();
+		if (!(((LivingEntity)(Object)this) instanceof Player)) return;
+		ServerLevel level = (ServerLevel) this.level();
+		double spread = level.getGameRules().get(DropSpread.DROP_SPREAD);
 
 		for (int i = 0; i < args.size(); i++)
 			args.set(i, (double) args.get(i) * spread);
